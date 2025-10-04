1b966484f468f4a7544a7ea6e1b21d1f31006d11
Owais Muhammad <owais.muhammad149@gmail.com>
2025-10-04 18:02:19 +0800

    fixed spelling issue

--- csrf-demo/2-transfer/attack.js
+++ csrf-demo/2-transfer/attack.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3003;
 
-// Repersents a basic attack script, typically would be clicked through a nefarious email 
+// Represents a basic attack script, typically would be clicked through a nefarious email 
 // or website. Takes advantage of preauthenticated session.
 app.get('/', (req, res) => {
   res.send(`
